c266571cea3ec7569f47944a7c6f6bfd8669dd80
Punkinut <heypunkinut@gmail.com>
2021-07-07 09:41:10 +1000

    Added config file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
-const mongoose = require("mongoose");
 const compression = require("compression");
+require("./config/connection");
 
 const PORT = 3000;
 
@@ -14,11 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
 
 // routes
 app.use(require("./routes/api.js"));

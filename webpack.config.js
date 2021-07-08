63859fe42582e6ab359823b080e53e8a4a260a16
Punkinut <heypunkinut@gmail.com>
2021-07-08 11:11:09 +1000

    Manifest plugin from webpack now generates a manifest file" "

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,31 @@ const config = {
       path: __dirname + "/public/dist",
       filename: "bundle.js",
     },
-    mode: "production"
+    mode: "production",
+    plugins: [
+      new WebpackPwaManifest({
+        filename: "manifest.json",
+        inject: false,
+        fingerprints: false,
+  
+        name: "Budget Tracker",
+        short_name: "Budget Tracker",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+  
+        icons: [
+          {
+            src: path.resolve(
+              __dirname,
+              "public/images/icons/icon-512x512.png"
+              ),
+            size: [32, 64, 128, 256, 512]
+          }
+        ]
+      })
+    ]
   };
   
   module.exports = config;

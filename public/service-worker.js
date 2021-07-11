9326caf7cf2da4d4fb3c19528b24a5d9461ebace
Punkinut <heypunkinut@gmail.com>
2021-07-11 13:19:32 +1000

    Added fetch handler in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,7 @@ const FILES_TO_CACHE = [
     "service-worker.js",
     "/dist/manifest.json",
     "/dist/bundle.js",
+    "/css/reset.css",
     "/css/style.css",
     "/images/close.png",
     "images/icons/icon-32x32.png",
@@ -12,7 +13,11 @@ const FILES_TO_CACHE = [
     "images/icons/icon-256x256.png",
     "images/icons/icon-512x512.png",
     "/js/db.js",
-    "/js/clicks.js"
+    "/js/clicks.js",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+    "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js",
+    "https://use.typekit.net/nwd3ukw.css",
+    "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap"
   ];
 
 
@@ -46,5 +51,50 @@ self.addEventListener("activate", event => {
         );
       })
       .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  // non GET requests are not cached and requests to other origins are not cached
+  if (
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  // handle runtime GET requests for data from /api routes
+  if (event.request.url.includes("/api/transaction")) {
+    // make network request and fallback to cache if network request fails (offline)
+    event.respondWith(
+      caches.open(RUNTIME_CACHE).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request));
+      })
+    );
+    return;
+  }
+
+  // use cache first for all other requests for performance
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // request is not in cache. make network request and cache the response
+      return caches.open(RUNTIME_CACHE).then(cache => {
+        return fetch(event.request).then(response => {
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
+          });
+        });
+      });
+    })
   );
 });

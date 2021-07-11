e565350f076af1113a98a15f370452aa38114184
Punkinut <heypunkinut@gmail.com>
2021-07-11 12:24:59 +1000

    Updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,18 @@
-console.log("Working!")
-
 const FILES_TO_CACHE = [
     "/",
-    "/manifest.json",
-    "/css/style.css",
-    "/js/db.js",
+    "/index.html",
+    "service-worker.js",
+    "/dist/manifest.json",
     "/dist/bundle.js",
+    "/css/style.css",
+    "/images/close.png",
+    "/icons/icon-32x32.png",
+    "/icons/icon-64x64.png",
+    "/icons/icon-128x128.png",
+    "/icons/icon-256x256.png",
+    "/icons/icon-512x512.png",
+    "/js/db.js",
+    "/js/clicks.js",
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
   ];
 
@@ -13,11 +20,11 @@ const FILES_TO_CACHE = [
 const STATIC_CACHE = "static-cache-v1";
 const RUNTIME_CACHE = "runtime-cache";
 
-// self.addEventListener("install", event => {
-//     event.waitUntil(
-//       caches
-//         .open(STATIC_CACHE)
-//         .then(cache => cache.addAll(FILES_TO_CACHE))
-//         .then(() => self.skipWaiting())
-//     );
-//   });
+self.addEventListener("install", event => {
+    event.waitUntil(
+      caches
+        .open(STATIC_CACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(() => self.skipWaiting())
+    );
+  });

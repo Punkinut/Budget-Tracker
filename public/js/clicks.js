74c8178adc3189d35e87b0601f22bb9799b4f412
Punkinut <heypunkinut@gmail.com>
2021-07-11 11:35:03 +1000

    Added a transaction will now hide the tab

--- public/js/clicks.js
+++ public/js/clicks.js
@@ -9,3 +9,13 @@ $('.add').on('click', () => {
     // Open Menu to add transaction
     $('.add-container').css('display', 'block');
 })
+
+$('#add-btn').on('click', () => {
+    // Open Menu to add transaction
+    $('.add-container').css('display', 'none');
+})
+
+$('#sub-btn').on('click', () => {
+    // Open Menu to add transaction
+    $('.add-container').css('display', 'none');
+})
